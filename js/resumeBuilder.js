81ec16a5755f3f650e83e9d9e318f5a446013308
Sean Holcomb <seanholcomb@gmail.com>
2015-12-11 00:42:09 -0800

    feat: Encapslate script

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,154 +1,166 @@
-var bio = {
-	"name" : "Sean Holcomb",
-	"role" : "Web Developer",
-	"contacts" : {
-		"mobile" : "(858)449-5236",
-		"email" : "seanholcomb@gmail.com",
-		"github" : "sean-holcomb",
-		"location" : "San Diego"
-	},
-	"skills" : [
-		"JavaScript", "Python", "Java", "SQL", "Android", "HTML5", "CSS3"
-	],
-	"bioPic": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAL3AAAAJGQ1ZmUyMTc3LWYyZDAtNDBiNi05ZGI0LWY2ODgyNzM5YmZjYw.jpg",
-	"message" : "Welcome to my resume."
-};
+function displayHeader(){
 
-var projects = {
-	"project" : [
-		{
-			"title" : "Ipsum Lorum",
-			"dates" : "November 2015",
-			"desc" : "Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum",
-			"images" : []
-		}
-	]
-}
-
-var work = {
-	"jobs" : [
-		{
-			"title" : "Waiter",
-			"employer" : "Ki's Restaurant",
-			"dates" : "October 2014 - December 2015",
-			"description" : "Take orders and ensure quility dinning experience for customers."
-		}
-	]
-};
-
-var education = {
-	"schools" : [
-		{
-			"name" : "University of California Santa Cruz",
-			"location" : "Santa Cruz, CA, US",
-			"dates" : "2010-2013",
-			"degree" : "B.A.",
-			"major" : "History"
-		}
-	],
-	"onlineCourses" : [
-		{
-			"title" : "Android Developer Nanodegree",
-			"onlineSchool" : "Udacity",
-			"dates" : "June 2015 - November 2015",
-			"url" : "Udacity.com"
+	var bio = {
+		"name" : "Sean Holcomb",
+		"role" : "Web Developer",
+		"contacts" : {
+			"mobile" : "(858)449-5236",
+			"email" : "seanholcomb@gmail.com",
+			"github" : "sean-holcomb",
+			"location" : "San Diego"
 		},
-		{
-			"title" : "Front-End Web Developer Nanodegree",
-			"onlineSchool" : "Udacity",
-			"dates" : "November 2015 - January 2015",
-			"url" : "Udacity.com"
-		},
-		{
-			"title" : "Full Stack Web Developer Nanodegree",
-			"onlineSchool" : "Udacity",
-			"dates" : "November 2015 - January 2015",
-			"url" : "Udacity.com"
+		"skills" : [
+			"JavaScript", "Python", "Java", "SQL", "Android", "HTML5", "CSS3"
+		],
+		"bioPic": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAL3AAAAJGQ1ZmUyMTc3LWYyZDAtNDBiNi05ZGI0LWY2ODgyNzM5YmZjYw.jpg",
+		"message" : "Welcome to my resume."
+	};
+
+	var formattedName = HTMLheaderName.replace("%data%", bio.name);
+	var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+	var formattedPic = HTMLbioPic.replace("%data%", bio.bioPic);
+	var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+	var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+	var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+
+	$("#header").prepend(formattedRole);
+	$("#header").prepend(formattedName);
+	$("#header").append(formattedPic);
+	$("#topContacts").append(formattedEmail);
+	$("#topContacts").append(formattedMobile);
+	$("#topContacts").append(formattedGithub);
+	if(bio.skills.length > 0) {
+		$("#header").append(HTMLskillsStart);
+		var formattedSkill;
+		for (var skill in bio.skills){
+			formattedSkill = HTMLskills.replace("%data%", bio.skills[skill]);
+			$("#skills").append(formattedSkill);
 		}
-	]
-}
-
-var formattedName = HTMLheaderName.replace("%data%", bio.name);
-var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-var formattedPic = HTMLbioPic.replace("%data%", bio.bioPic);
-var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
-var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
-var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
-
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-$("#header").append(formattedPic);
-$("#topContacts").append(formattedEmail);
-$("#topContacts").append(formattedMobile);
-$("#topContacts").append(formattedGithub);
-if(bio.skills.length > 0) {
-	$("#header").append(HTMLskillsStart);
-	var formattedSkill;
-	for (var skill in bio.skills){
-		formattedSkill = HTMLskills.replace("%data%", bio.skills[skill]);
-		$("#skills").append(formattedSkill);
 	}
 }
 
-for (i in projects.project){
-	$("#projects").append(HTMLprojectStart);
+function displayProjects(){
 
-	var formTitle = HTMLprojectTitle.replace("%data%", projects.project[i].title);
-	var formDates = HTMLprojectDates.replace("%data%", projects.project[i].dates);
-	var formDesc = HTMLprojectDescription.replace("%data%", projects.project[i].desc);
+	var projects = {
+		"project" : [
+			{
+				"title" : "Ipsum Lorum",
+				"dates" : "November 2015",
+				"desc" : "Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum Ipsum Lorum",
+				"images" : []
+			}
+		]
+	}
 
-	$(".project-entry:last").append(formTitle);
-	$(".project-entry:last").append(formDates);
-	$(".project-entry:last").append(formDesc);
-	for (pic in projects.project[i].images){
-		var formImage = HTMLprojectImage.replace("%data%", projects.project[i].images[pic]);
-		$(".project-entry:last").append(formImage);
+	for (i in projects.project){
+		$("#projects").append(HTMLprojectStart);
+
+		var formTitle = HTMLprojectTitle.replace("%data%", projects.project[i].title);
+		var formDates = HTMLprojectDates.replace("%data%", projects.project[i].dates);
+		var formDesc = HTMLprojectDescription.replace("%data%", projects.project[i].desc);
+
+		$(".project-entry:last").append(formTitle);
+		$(".project-entry:last").append(formDates);
+		$(".project-entry:last").append(formDesc);
+		for (pic in projects.project[i].images){
+			var formImage = HTMLprojectImage.replace("%data%", projects.project[i].images[pic]);
+			$(".project-entry:last").append(formImage);
+		}
 	}
 }
 
-for (job in work.jobs){
-	$("#workExperience").append(HTMLworkStart);
+function displayWork(){
 
-	var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-	var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
-	var formattedEmployerTitle = formattedEmployer + formattedTitle;
-	var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
-	var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+	var work = {
+		"jobs" : [
+			{
+				"title" : "Waiter",
+				"employer" : "Ki's Restaurant",
+				"dates" : "October 2014 - December 2015",
+				"description" : "Take orders and ensure quility dinning experience for customers."
+			}
+		]
+	};
 
-	$(".work-entry:last").append(formattedEmployerTitle);
-	$(".work-entry:last").append(formattedDates);
-	$(".work-entry:last").append(formattedDescription);
+	for (job in work.jobs){
+		$("#workExperience").append(HTMLworkStart);
 
+		var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+		var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+		var formattedEmployerTitle = formattedEmployer + formattedTitle;
+		var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+		var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+
+		$(".work-entry:last").append(formattedEmployerTitle);
+		$(".work-entry:last").append(formattedDates);
+		$(".work-entry:last").append(formattedDescription);
+	}
 }
 
-for (school in education.schools){
-	$("#education").append(HTMLschoolStart);
+function displayEducation(){
 
-	var formName = HTMLschoolName.replace("%data%", education.schools[school].name);
-	var formDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
-	var formDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
-	var formLoc = HTMLschoolLocation.replace("%data%", education.schools[school].location);
-	var formMajor = HTMLschoolMajor.replace("%data%", education.schools[school].major);
-	var fromMajorDeg = formMajor + formDegree;
+	var education = {
+		"schools" : [
+			{
+				"name" : "University of California Santa Cruz",
+				"location" : "Santa Cruz, CA, US",
+				"dates" : "2010-2013",
+				"degree" : "B.A.",
+				"major" : "History"
+			}
+		],
+		"onlineCourses" : [
+			{
+				"title" : "Android Developer Nanodegree",
+				"onlineSchool" : "Udacity",
+				"dates" : "June 2015 - November 2015",
+				"url" : "Udacity.com"
+			},
+			{
+				"title" : "Front-End Web Developer Nanodegree",
+				"onlineSchool" : "Udacity",
+				"dates" : "November 2015 - January 2015",
+				"url" : "Udacity.com"
+			},
+			{
+				"title" : "Full Stack Web Developer Nanodegree",
+				"onlineSchool" : "Udacity",
+				"dates" : "November 2015 - January 2015",
+				"url" : "Udacity.com"
+			}
+		]
+	}
 
-	$(".education-entry:last").append(formName);
-	$(".education-entry:last").append(formLoc);
-	$(".education-entry:last").append(formDates);
-	$(".education-entry:last").append(fromMajorDeg);
-}
 
-for (oClass in education.onlineCourses){
-	$("#education").append(HTMLschoolStart);
+	for (school in education.schools){
+		$("#education").append(HTMLschoolStart);
 
-	var formTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[oClass].title);
-	var formSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[oClass].onlineSchool);
-	var formDates = HTMLonlineDates.replace("%data%", education.onlineCourses[oClass].dates);
-	var formUrl = HTMLonlineURL.replace("%data%", education.onlineCourses[oClass].url);
-	var formFull = formTitle + formSchool;
+		var formName = HTMLschoolName.replace("%data%", education.schools[school].name);
+		var formDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+		var formDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
+		var formLoc = HTMLschoolLocation.replace("%data%", education.schools[school].location);
+		var formMajor = HTMLschoolMajor.replace("%data%", education.schools[school].major);
+		var fromMajorDeg = formMajor + formDegree;
 
-	$(".education-entry:last").append(formFull);
-	$(".education-entry:last").append(formDates);
-	$(".education-entry:last").append(formUrl);
+		$(".education-entry:last").append(formName);
+		$(".education-entry:last").append(formLoc);
+		$(".education-entry:last").append(formDates);
+		$(".education-entry:last").append(fromMajorDeg);
+	}
+
+	for (oClass in education.onlineCourses){
+		$("#education").append(HTMLschoolStart);
+
+		var formTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[oClass].title);
+		var formSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[oClass].onlineSchool);
+		var formDates = HTMLonlineDates.replace("%data%", education.onlineCourses[oClass].dates);
+		var formUrl = HTMLonlineURL.replace("%data%", education.onlineCourses[oClass].url);
+		var formFull = formTitle + formSchool;
+
+		$(".education-entry:last").append(formFull);
+		$(".education-entry:last").append(formDates);
+		$(".education-entry:last").append(formUrl);
+	}
 }
 
 function InternationalizeName(oldName) {
@@ -160,3 +172,8 @@ function InternationalizeName(oldName) {
     finalName=name.join(" ");
     return finalName;
 }
+
+displayHeader();
+displayProjects();
+displayWork();
+displayEducation();
